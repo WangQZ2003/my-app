0b370eedffa2f8c4bd5327f3616fdaf87b834917
wqz <2581284161@qq.com>
2024-05-17 20:36:26 +0800

    vue3

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,14 @@ export default defineConfig({
     vue(),
     vueJsx(),
   ],
+  server:{
+    //端口号
+    port:8080,
+    // 是否自动打开浏览器
+    open:true,
+    // 主机号
+    host: 'localhost',
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
